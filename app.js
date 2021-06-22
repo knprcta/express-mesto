1c31a0d71ca03f3ef7e0983e451635631784714c
Kirill Fedotov <knprcta@yandex.ru>
2021-06-22 22:28:27 +0300

    feat: добавить схемы, модели, роуты и контроллеры для пользователей

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const usersRouter = require('./routes/users');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+
+app.use('/users', usersRouter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
